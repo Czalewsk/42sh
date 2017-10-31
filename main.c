eda9c89a1af2900205a7648a6e3d33542c4ceffa
Dricc <czalewsk@student.42.fr>
2017-10-31 13:06:17 +0100

    intercept all key on keyboard and print

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: czalewsk <czalewsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/27 14:59:56 by czalewsk          #+#    #+#             */
-/*   Updated: 2017/10/31 08:55:13 by czalewsk         ###   ########.fr       */
+/*   Updated: 2017/10/31 13:00:44 by czalewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,12 @@
 #include <term.h>
 #include <stdlib.h>
 #include <wctype.h>
+#include <string.h>
+#include <errno.h>
+
 
 #define CTRL_KEY(k) ((k) & 0x1f)
+#define SIZE_OF_READ (8)
 
 char	*termcap = NULL;
 struct termios s_termios;
@@ -42,69 +46,19 @@ char	init_termcap(void)
 	return (ret);
 }
 
-void testTextColorAndEffect()
+void	restore_tty_param(void)
 {
-	char *af_cmd = tgetstr("AF", &termcap);
-	char *reset_cmd = tgetstr("me", &termcap);
-	char *blink_cmd = tgetstr("mb", &termcap);
-	char *bold_cmd = tgetstr("md", &termcap);
-	char *underline_cmd = tgetstr("us", &termcap);
-
-	tputs(tparm(af_cmd, COLOR_RED), 1, putchar);
-	printf("Texte rouge\n");
-
-	tputs(tparm(af_cmd, COLOR_GREEN), 1, putchar);
-	printf("Texte vert\n");
-
-	tputs(tparm(af_cmd, COLOR_BLUE), 1, putchar);
-	printf("Texte bleu\n");
-
-	tputs(reset_cmd, 1, putchar);
-	tputs(blink_cmd, 1, putchar);
-	printf("Texte clignotant\n");
-
-	tputs(tparm(af_cmd, COLOR_GREEN), 1, putchar);
-	printf("Texte vert clignotant\n");
-
-	tputs(reset_cmd, 1, putchar);
-	tputs(bold_cmd, 1, putchar);
-	printf("Texte en gras\n");
-
-	tputs(tparm(af_cmd, COLOR_RED), 1, putchar);
-	printf("Texte rouge en gras\n");
-
-	tputs(reset_cmd, 1, putchar);
-	tputs(tparm(af_cmd, COLOR_GREEN), 1, putchar);
-	tputs(underline_cmd, 1, putchar);
-	printf("Texte vert souligné\n");
-
-	tputs(tparm(af_cmd, COLOR_BLUE), 1, putchar);
-	tputs(bold_cmd, 1, putchar);
-	tputs(blink_cmd, 1, putchar);
-	tputs(underline_cmd, 1, putchar);
-	printf("Texte blue en gras souligné clignotant\n");
-	tputs(reset_cmd, 1, putchar);
+	tcsetattr(0, 0, &s_termios_backup);
 }
 
-void	print_window(void)
+void	debug_key(char key[SIZE_OF_READ], int nb)
 {
-	int		co;
-	int		li;
-	int		i;
+	int i;
 
-	co = tgetnum("co");
-	li = tgetnum("li");
 	i = 0;
-	ft_printf("Nombre de ligne = %d | Nbr de col = %d\r\n", li, co);
-	while (++i <= co)
-		ft_putnbr(i % 10);
-	i = 2;
-	write(1, "\n", 1);
-	while (++i <= li)
-	{
-		ft_putnbr(i);
-		write(1, "\r\n", 2);
-	}
+	ft_printf("READ=%i\r\n", nb);
+	while (i < nb)
+		printf("%hhi\r\n", key[i++]);
 }
 
 void	set_tty(void)
@@ -118,29 +72,42 @@ void	set_tty(void)
 	s_termios.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 	s_termios.c_oflag &= ~(OPOST);
 	s_termios.c_cflag |= (CS8);
+	s_termios.c_cc[VMIN] = 0;
+	s_termios.c_cc[VTIME] = 1;
 //	s_termios.c_lflag &= ~(ECHO);
 	tcsetattr(0, 0, &s_termios);
 }
 
+int		read_key(char *c)
+{
+	int		nread;
+
+	while ((nread = read(0, c, SIZE_OF_READ))  <= 0)
+		if (nread == -1)
+			ft_error(strerror(errno), &restore_tty_param);
+	return (nread);
+}
+
+int		process_read_key(void)
+{
+	char	c[SIZE_OF_READ];
+	int		nread;
+
+	ft_bzero(&c, SIZE_OF_READ);
+	nread = read_key((char*)c);
+	debug_key(c, nread);  //DEBUG
+	if (nread == 1 && *c == CTRL_KEY('q'))
+		return (0);
+	return (1);
+}
+
 int		main(void)
 {
-	unsigned long int		i;
-
 	if (init_termcap() <= 0)
 		return (1);
-	print_window();
-//	testTextColorAndEffect();
 	set_tty();
 	while (1)
-	{
-		i = 0;
-		read(0, &i, sizeof(i));
-		ft_printf("READ=%d | char {{", i);
-		if (!iswcntrl(i))
-			write(1, &i, sizeof(i));
-		ft_putendl("}\r");
-		if (i == CTRL_KEY('q'))
-			return (0);
-	}
-	tcsetattr(0, 0, &s_termios_backup);
+		if (!process_read_key())
+			break ;
+	restore_tty_param();
 }
