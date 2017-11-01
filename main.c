212a6646aadf6335d34a034eae4b28fa6f7e020d
Dricc <czalewsk@student.42.fr>
2017-11-01 19:25:32 +0100

    implement archi

--- main.c
+++ main.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: czalewsk <czalewsk@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/10/27 14:59:56 by czalewsk          #+#    #+#             */
-/*   Updated: 2017/11/01 11:41:37 by czalewsk         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft/libft.h"
 #include <termcap.h>
 #include <curses.h>
@@ -22,66 +10,23 @@
 #include <locale.h>
 #include <wctype.h>
 
+/*
+** TERMINAL MODIFICATION GLOBAL STRUCTURE OF THE DEATH
+*/
 
-#define CTRL_KEY(k) ((k) & 0x1f)
-#define SIZE_OF_READ (12)
-
-char	*termcap = NULL;
 struct termios s_termios;
 struct termios s_termios_backup;
 
-char	*left_cap = NULL;
-char	*right_cap = NULL;
-char	*up_cap = NULL;
-char	*down_cap = NULL;
-char	*clear_cap = NULL;
-char	*nextline_cap = NULL;
-char	*home_cap = NULL;
-char	*test_cap = NULL;
+/*
+**	READ DEFINE OF BENYCHoOOU BRO DU GAME TU PESE
+*/
 
-void	init_term_cap(void)
-{
-	left_cap = tgetstr("le", NULL);
-	right_cap = tgetstr("nd", NULL);
-	down_cap = tgetstr("do", NULL);
-	up_cap = tgetstr("up", NULL);
-	clear_cap = tgetstr("cl", NULL);
-	nextline_cap = tgetstr("sf", NULL);
-	tputs(tgetstr("sc", NULL), 0, &ft_putchar_termcap);
-	home_cap = tgetstr("rc", NULL);
-	test_cap = tgetstr("cm", NULL);
-	if (left_cap && down_cap && up_cap && right_cap && clear_cap && nextline_cap && 
-			home_cap && test_cap)
-		ft_printf("TERMCAP CAPABILITY DONE\r\n");
-}
+#define SIZE_READ (12)
+#define CTRL_KEY(k) ((k) & 0x1f)
 
-char	init_termcap(void)
-{
-	int		ret;
-	char	*env_term;
+#define PROMPT ("Cedychou_sh 🦆  ")
 
-	if(!(env_term = getenv("TERM")))
-	{
-		ft_printf("NO TERM IN ENV\n");
-		return (-1);
-	}
-	termcap = NULL;
-	ret = tgetent(termcap, env_term);
-	if (ret < 0)
-		ft_printf("Could not access the termcaps database\n");
-	else if (!ret)
-		ft_printf("Terminale %s is not documented\n", env_term);
-	if (ret)
-		init_term_cap();
-	return (ret);
-}
-
-void	restore_tty_param(void)
-{
-	tcsetattr(0, 0, &s_termios_backup);
-}
-
-void	debug_key(char key[SIZE_OF_READ], int nb)
+void	debug_key(char key[SIZE_READ], int nb)
 {
 	int i;
 
@@ -89,6 +34,11 @@ void	debug_key(char key[SIZE_OF_READ], int nb)
 	ft_printf("READ=%i\r\n", nb);
 	while (i < nb)
 		printf("%hhi\r\n", key[i++]);
+}
+
+void	restore_tty_param(void)
+{
+	tcsetattr(0, 0, &s_termios_backup);
 }
 
 void	set_tty(void)
@@ -108,62 +58,76 @@ int		read_key(char *c)
 {
 	int		nread;
 
-	while ((nread = read(0, c, SIZE_OF_READ))  <= 0)
+	while ((nread = read(0, c, SIZE_READ))  <= 0)
 		if (nread == -1)
-			ft_error(strerror(errno), &restore_tty_param);
+			ft_error(strerror(errno), &restore_tty_param); // A recoder :D
 	return (nread);
 }
 
-void	handle_arrow_key(char key)
+/*
+**  Gere l'affichage et le deplacement du cursor (retour a la ligne...)
+*/
+
+void	sh_print_char(int nread, char entry[SIZE_READ])
 {
-	if (key == 68)
-		tputs(left_cap, 0, &ft_putchar_termcap);
-	if (key == 67)
-		tputs(right_cap, 0, &ft_putchar_termcap);
-	if (key == 72)
-		tputs(home_cap, 0, &ft_putchar_termcap);
-//	if (key == 65)
-//		tputs(up_cap, 0, &ft_putchar_termcap);
-//	if (key == 66)
-//		tputs(down_cap, 0, &ft_putchar_termcap);
+	return ;
 }
 
-void	handler_cntrl_key(int nread, char c[SIZE_OF_READ])
+/*
+** Gere les differentes combinaisons de touches avec CONTROL
+*/
+
+char	cntrl_handler(int nread, char entry[SIZE_READ])
 {
-	if (nread == 3 && c[0] == 27 && c[1] == 91)
-		handle_arrow_key(c[2]);
-	if (nread == 1 && *c == CTRL_KEY('k'))
-		tputs(clear_cap, 0, &ft_putchar_termcap);
-	if (nread == 1 && *c == 13)
-		tputs(nextline_cap, 1, &ft_putchar_termcap);
-	if (nread == 1 && *c == CTRL_KEY('x'))
-		tputs(tgoto(test_cap, 1, 1), 1, &ft_putchar_termcap);
+	return (0);
 }
 
-int		process_read_key(void)
+/*
+** Switch entre les differents key handlers
+*/
+
+char	key_wrapper(int nread, char entry[SIZE_READ])
 {
-	char	c[SIZE_OF_READ];
+
+	return (0);
+}
+
+unsigned char	read_entry(char **cmd)
+{
 	int		nread;
+	char	entry[SIZE_READ];
+	char	a;
 
-	ft_bzero(&c, SIZE_OF_READ);
-	nread = read_key((char*)c);
-//	debug_key(c, nread);  //DEBUG
-	if (nread == 1 && *c == CTRL_KEY('q'))
+	(void)cmd;
+	nread = read_key((char*)entry);
+	if (nread == 1 && CTRL_KEY('a') == *entry)
 		return (0);
-	if (nread <= sizeof(wint_t) && !ft_iswcntrl((int)*c))
-		write(1, c, nread);
+	if (nread <= sizeof(wint_t) && !ft_iswcntrl((int)*entry))
+		sh_print_char(nread, entry);
 	else
-		handler_cntrl_key(nread, c);
-	return (1);
+		a = key_wrapper(nread, entry); // Switch entre les differents gestionnaires de touches
+	return (a);
+}
+
+void	sh_prompt(void)
+{
+	ft_putstr(PROMPT);
 }
 
 int		main(void)
 {
-	if (init_termcap() <= 0)
-		return (1);
+	char			*cmd;
+	unsigned char	ret;
+
+	cmd = NULL;
 	set_tty();
-	while (1)
-		if (!process_read_key())
+	sh_prompt(); // Envoyer la valeur de ret et include dans la boucle
+	while (42)
+	{
+		// A remplacer par une fonction qui return uniquement quand elle recoit un \n
+		if (!(ret = read_entry(&cmd)))
 			break ;
+		// Execute a ret = cmd(&cmd);
+	}
 	restore_tty_param();
 }
--- test.sh
+++
@@ -1,5 +0,0 @@
-for i in `seq 1 89`;
-do
-	let "a = i % 10"
-	 echo -n $a
-		done
