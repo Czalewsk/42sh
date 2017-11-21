3a859bfcbaf4836ea19073431fe4f6dec218c8f6
Dricc <czalewsk@student.42.fr>
2017-11-21 21:07:46 +0100

    home end + full space -> broken

--- srcs/edition/unicode.c
+++ srcs/edition/unicode.c
@@ -6,7 +6,7 @@
 /*   By: czalewsk <czalewsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/21 10:25:42 by czalewsk          #+#    #+#             */
-/*   Updated: 2017/11/21 18:00:42 by czalewsk         ###   ########.fr       */
+/*   Updated: 2017/11/21 20:57:58 by czalewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,24 +24,21 @@ int		sh_curs_unicode(char *str, int index, int end)
 	unsigned char	test;
 	int				char_visu;
 
-	DEBUG("index = %d(end=%d)(len=%d)\n~%s~\n", index, end, ft_strlen(str), str)
 	start_char = 0;
 	i = 0;
 	char_visu = 0;
 	test = 0;
-	while (((char_visu <= index) || (end && start_char != i - 1)) && (test = *(str + i)))
+	while (((char_visu <= index) 
+				|| (end && start_char != i - 1)) && (test = *(str + i)))
 	{
 		if ((test & 0xC0) != 0x80 || ((test & 0xE0) == 0xC0))
 		{
-			DEBUG("\t\tWHILE=%s|\n", str + i)
 			start_char = i;
 			char_visu++;
 		}
-		DEBUG("visu=%d | test=%#X\n", start_char, test)
 		i++;
 	}
 	if (!test)
 		start_char = i;
-	DEBUG("Index unicode =%d\n", start_char);
 	return (start_char);
 }
