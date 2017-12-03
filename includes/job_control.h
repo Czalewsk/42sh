26a46b9032b1dd98e741da952727b682245fc8d5
Stephane CORBION <scorbion@e2r9p12.42.fr>
2017-12-03 18:52:32 +0100

    premice du job control

---
+++ includes/job_control.h
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   job_control.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scorbion <scorbion@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/03 12:53:03 by scorbion          #+#    #+#             */
+/*   Updated: 2017/12/03 17:18:56 by scorbion         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef JOB_CONTROL_H
+# define JOB_CONTROL_H
+
+# include <sys/types.h>
+# include <termios.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <errno.h>
+
+/* A process is a single process.  */
+typedef struct	s_process
+{
+	struct s_process	*next;      /* next process in pipeline */
+	char				**argv;		/* for exec */
+	pid_t				pid;        /* process ID */
+	char				completed;  /* true if process has completed */
+	char				stopped;    /* true if process has stopped */
+	int					status;     /* reported status value */
+}				t_process;
+
+/* A job is a pipeline of processes.  */
+typedef struct	s_job
+{
+	struct s_job	*next;			/* next active job */
+	char			*command;		/* command line, used for messages */
+	t_process		*first_process;	/* list of processes in this job */
+	pid_t			pgid;			/* process group ID */
+	char			notified;		/* true if user told about stopped job */
+	struct termios	tmodes;			/* saved terminal modes */
+	int				stdin;  		/* standard i/o channels */
+	int				stdout;
+	int				stderr;
+}				t_job;
+
+extern t_job *first_job;
+extern pid_t shell_pgid;
+extern struct termios shell_tmodes;
+extern int shell_terminal;
+extern int shell_is_interactive;
+
+t_job	*find_job(pid_t pgid);
+int		job_is_stopped(t_job *j);
+int		job_is_completed(t_job *j);
+void	continue_job (t_job *j, int foreground);
+void	do_job_notification (void);
+void	format_job_info (t_job *j, const char *status);
+void	init_shell ();
+void	launch_job (t_job *j, int foreground);
+void	launch_process (t_process *p, pid_t pgid,
+                int infile, int outfile, int errfile,
+                int foreground);
+void	mark_job_as_running (t_job *j);
+int		mark_process_status (pid_t pid, int status);
+void	put_job_in_background (t_job *j, int cont);
+void	put_job_in_foreground (t_job *j, int cont);
+void	update_status (void);
+void	wait_for_job (t_job *j);
+
+
+
+
+
+#endif
---
+++ srcs/jobControl/init_shell.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_shell.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scorbion <scorbion@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/03 16:22:18 by scorbion          #+#    #+#             */
+/*   Updated: 2017/12/03 18:35:40 by scorbion         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/job_control.h"
+
+t_job *first_job;
+pid_t shell_pgid;
+struct termios shell_tmodes;
+int shell_terminal;
+int shell_is_interactive;
+
+/* Make sure the shell is running interactively as the foreground job
+   before proceeding. */
+
+void  init_shell (void)
+{
+
+  /* See if we are running interactively.  */
+  shell_terminal = STDIN_FILENO;
+  shell_is_interactive = isatty (shell_terminal);
+
+  if (shell_is_interactive)
+    {
+      /* Loop until we are in the foreground.  */
+      while (tcgetpgrp (shell_terminal) != (shell_pgid = getpgrp ()))
+        kill (- shell_pgid, SIGTTIN);
+
+      /* Ignore interactive and job-control signals.  */
+      signal (SIGINT, SIG_IGN);
+      signal (SIGQUIT, SIG_IGN);
+      signal (SIGTSTP, SIG_IGN);
+      signal (SIGTTIN, SIG_IGN);
+      signal (SIGTTOU, SIG_IGN);
+      signal (SIGCHLD, SIG_IGN);
+
+      /* Put ourselves in our own process group.  */
+      shell_pgid = getpid ();
+      if (setpgid (shell_pgid, shell_pgid) < 0)
+        {
+          perror ("Couldn't put the shell in its own process group");
+          exit (1);
+        }
+
+      /* Grab control of the terminal.  */
+      tcsetpgrp (shell_terminal, shell_pgid);
+
+      /* Save default terminal attributes for shell.  */
+      tcgetattr (shell_terminal, &shell_tmodes);
+    }
+}
+
+int main (void)
+{
+  return (0);
+}
