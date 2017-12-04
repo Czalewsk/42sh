80e80cfec1c242854b078c8fe5503f83891c739e
Dricc <czalewsk@student.42.fr>
2017-12-04 21:25:51 +0100

    escape function works for Bslash

--- sh_escape.c
+++ sh_escape.c
@@ -6,11 +6,13 @@
 /*   By: czalewsk <czalewsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/02 20:23:05 by czalewsk          #+#    #+#             */
-/*   Updated: 2017/12/04 15:02:40 by czalewsk         ###   ########.fr       */
+/*   Updated: 2017/12/04 21:23:33 by czalewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static void *escape_fonctions(char c, char *escaped, char *to_remove, int index);
 
 const char		g_charset[] = "\"\'\\";
 unsigned int	g_nb_remove = 0;
@@ -20,7 +22,38 @@ static inline void	set_escape(char *str, unsigned int index)
 	*(str + index / 8) |= (1 << (index % 8));
 }
 
-void *sh_esc_bslash(char c, char *escaped, char *to_remove, int index)
+char			sh_is_escape(char *str, unsigned int index)
+{
+	return (*(str + index / 8) & (1 << (index % 8)));
+}
+
+static void			remove_escape(char *str, char *to_remove)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+		ft_printf("%X\n", *to_remove);
+	while (*str)
+	{
+		if (sh_is_escape(to_remove, i))
+		{
+			ft_printf("i=%d\n", i);
+			*str = *(str + 1 + j);
+//			if (!*str)
+//				break ;
+			j++;
+			i++;
+			continue ;
+		}
+		(*str = *(str + j));
+		str++;
+		i++;
+	}
+}
+
+void	*sh_esc_bslash(char c, char *escaped, char *to_remove, int index)
 {
 	(void)c;
 	(void)to_remove;
@@ -36,9 +69,9 @@ static void *escape_fonctions(char c, char *escaped, char *to_remove, int index)
 	(void)escaped;
 	if ((res = ft_strchr(g_charset, c)))
 	{
+		set_escape(to_remove, index);
 		++g_nb_remove;
-		set_escape(to_remove, index - g_nb_remove);
-		return ((void *)fcts[(int)(res - g_charset) + 1]);
+		return ((void *)fcts[(int)(res - g_charset)]);
 	}
 	else
 		return (&escape_fonctions);
@@ -62,5 +95,17 @@ char		sh_escape(char *str, char **escaped)
 		if (!(f = f(*(str + i), *escaped, to_remove, i)))
 			ret = 1;
 	remove_escape(str, to_remove);
+	ft_strdel(&to_remove);
 	return (ret);
+}
+
+
+int		main(void)
+{
+	char str[] = "\\l\\o\\\\l";
+	char *l = NULL;
+
+	ft_printf("Avant=%s|\n", str);
+	sh_escape(str, &l);
+	ft_printf("Apres=%s|\n", str);
 }
