677c546cf9771a0be71c0111704fe9439156d232
Dricc <czalewsk@student.42.fr>
2017-12-11 20:51:58 +0100

    better research in bracket expansion

--- srcs/glob/brace_expansion.c
+++ srcs/glob/brace_expansion.c
@@ -6,25 +6,26 @@
 /*   By: czalewsk <czalewsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/08 13:23:30 by czalewsk          #+#    #+#             */
-/*   Updated: 2017/12/11 18:13:42 by czalewsk         ###   ########.fr       */
+/*   Updated: 2017/12/11 20:51:17 by czalewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_sh.h"
 #include "sh_glob.h"
 
-void		sh_find_beg(char **curs, char **begin, char **end, int *state)
+char	sh_find_beg(char **curs, char **begin, char **end)
 {
 	if (**curs == '{')
 	{
 		*end = NULL;
 		*begin = *curs;
 		(*curs)++;
-		*state = 1;
+		return (1);
 	}
+	return (0);
 }
 
-int		sh_find_end(char **curs, char **begin, char **end)
+char	sh_find_end(char **curs, char **begin, char **end)
 {
 	(void)begin;
 	if (**curs == '}')
@@ -32,37 +33,71 @@ int		sh_find_end(char **curs, char **begin, char **end)
 		*end = *curs;
 		return (2);
 	}
+	if (!**curs)
+	{
+		*curs = *begin;
+		*end = NULL;
+		*begin = NULL;
+		return (0);
+	}
 	return (1);
 }
 
-int		brace_exp_valide_type(char **curs, char **begin, char **end)
+char	brace_exp_choice(char *curs, char *end)
 {
-	/* Retourne 2 ou 3 si VALIDE sinon 1 */
-	if (brace_exp_choice(curs, begin, end))
-		return (2);
-	else if (brace_exp_seq(curs, begin, end))
+	int		forbidden;
+
+	forbidden = 0;
+	while (++curs < end)
+	{
+		if (*curs == '\\')
+			curs += (*(curs + 1) && *(curs + 2)) ? 2 : 1;
+		if (*curs == ',')
+			forbidden++;
+		if (*curs == ' ' || *curs == '{')
+			return (0);
+	}
+		return (forbidden > 0 ? 1 : 0);
+}
+
+char	brace_exp_seq(char *curs, char *end)
+{
+	(void)curs;
+	(void)end;
+	return (0);
+}
+
+char	brace_exp_valide_type(char **curs, char **begin, char **end)
+{
+	(void)curs;
+	/* Retourne 3 ou 4 si VALIDE sinon 1 */
+	if (brace_exp_choice(*begin, *end))
 		return (3);
+	else if (brace_exp_seq(*begin, *end))
+		return (4);
+	*curs = *end;
+	*end = NULL;
 	return (1);
 }
 
 char	glob_find_pattern(char **curs, char **begin, char **end)
 {
 	char		*tmp;
-	int			state;
+	char		state;
+	char		(*const f[3])(char **, char **, char **) =
+	{&sh_find_beg, &sh_find_end, &brace_exp_valide_type};
 
 	tmp = *curs;
 	*begin = NULL;
 	state = 0;
 	*end = NULL;
-	while (*tmp && state < 2)
+	while (42)
 	{
 		if (*tmp == '\\')
 			tmp += (*(tmp + 1) && *(tmp + 2)) ? 2 : 1;
-		sh_find_beg(&tmp, begin, end, &state);
-		if (state)
-			sh_find_end(&tmp, begin, end);
-		if (*end)
-			state = brace_exp_valide_type(&tmp, begin, end);
+		state = f[(int)state](&tmp, begin, end);
+		if (state > 2 || !*tmp)
+			break ;
 		tmp++;
 	}
 	*curs = tmp;
