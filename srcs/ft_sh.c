0137b3b97756299fe9022256aa1aaff7194ec6a1
Dricc <czalewsk@student.42.fr>
2018-01-04 18:51:37 +0100

    remove debug in main

--- srcs/ft_sh.c
+++ srcs/ft_sh.c
@@ -6,7 +6,7 @@
 /*   By: bviala <bviala@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/06 16:10:34 by czalewsk          #+#    #+#             */
-/*   Updated: 2018/01/04 15:40:07 by czalewsk         ###   ########.fr       */
+/*   Updated: 2018/01/04 18:49:03 by czalewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,19 +40,6 @@ int			main(int ac, char **av, char **env)
 		if (ret == -3)
 			continue ;
 		DEBUG("\r\nCMD=|%s|", cmd.cmd);
-	/* TEST GLOB FIND PATTERN	*/
-	t_list *resultat = sh_brace_exp(cmd.cmd);
-	t_list *tmp;
-	tmp = resultat;
-	write(1,"\n",1);
-	while (tmp)
-	{
-		ft_printf(" %s", tmp->content);
-		tmp = tmp->next;
-	}
-	ft_lstclear(&resultat);
-	write(1,"\n",1);
-	/* FIN DU TEST*/
 		ft_strdel(&cmd.cmd);
 	}
 	ft_strdel(&cmd.cmd);
