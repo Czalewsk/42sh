e83a7acd19816045dfb3bab60f55bffaa6b8ca28
Dricc <czalewsk@student.42.fr>
2018-01-30 14:44:58 +0100

    checkout master termcaps files

--- includes/termcaps.h
+++ includes/termcaps.h
@@ -6,7 +6,7 @@
 /*   By: czalewsk <czalewsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/06 15:29:26 by czalewsk          #+#    #+#             */
-/*   Updated: 2018/01/29 18:55:38 by czalewsk         ###   ########.fr       */
+/*   Updated: 2017/11/06 17:41:40 by czalewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,10 @@
 
 typedef enum	e_termcaps_capability
 {
-	LEFT, RIGHT, DOWN, UP_CAP, COL, NDO, NUP, CLEAR, UNDER_START, UNDER_STOP,
-	LAST_CAPS
+	LEFT, RIGHT, DOWN, UP_CAP, COL, NDO, NUP, CLEAR
 }				t_termcaps_capability;
 
-char	*g_termcaps_cap[LAST_CAPS];
+char	*g_termcaps_cap[8];
 
 char		termcaps_init(char **env);
 void		termcaps_restore_tty(void);
