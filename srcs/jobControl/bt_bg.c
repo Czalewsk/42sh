956a34c458a11606f57c69cd348580e3e8474b42
Stephane CORBION <scorbion@e2r9p12.42.fr>
2018-02-24 10:03:05 +0100

    gestion du multi argument passe a bg

--- srcs/jobControl/bt_bg.c
+++ srcs/jobControl/bt_bg.c
@@ -6,25 +6,35 @@
 /*   By: scorbion <scorbion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/17 16:18:10 by scorbion          #+#    #+#             */
-/*   Updated: 2018/02/17 17:58:52 by scorbion         ###   ########.fr       */
+/*   Updated: 2018/02/24 09:59:45 by scorbion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/job_control.h"
 
-int bt_bg(char *arg)
+int bt_bg(char **arg)
 {
     t_job   *tmp;
-    
-    tmp = get_job(arg);
-    if (tmp == NULL)
+    int     i;
+    int     retour;
+
+    i = 0;
+    retour = 0;
+    if (*arg == NULL)
     {
-        if (arg == NULL)
-            //42sh: bg: current: no such job
-        else
-            //42sh: bg: arg: no such job
-        return (1);
+        //42sh: bg: current: no such job
+        retour = 1;
     }
-    continue_job(tmp, 0);
-    return (0);
+    while (arg[i])
+    {
+        tmp = get_job(arg);
+        if (tmp == NULL)
+        {
+            //42sh: bg: arg: no such job
+            retour = 1;
+        }
+        continue_job(tmp, 0);
+        i++;
+    }
+    return (retour);
 }
