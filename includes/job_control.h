8c8581fd5c0c3a08f9c21b8eadcc399fe2a69daa
Stephane CORBION <scorbion@e2r9p12.42.fr>
2018-02-24 11:24:41 +0100

    new list of my function

--- includes/job_control.h
+++ includes/job_control.h
@@ -6,7 +6,7 @@
 /*   By: scorbion <scorbion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/03 12:53:03 by scorbion          #+#    #+#             */
-/*   Updated: 2018/02/24 10:09:40 by scorbion         ###   ########.fr       */
+/*   Updated: 2018/02/24 11:15:32 by scorbion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,28 @@ pid_t			shell_pgid;
 struct termios	shell_tmodes;
 int				shell_terminal;
 int				shell_is_interactive;
+
+
+
+
+void    put_first_in_job_order(t_job *j) /* Permet de trouver le conteneur list du job en parametre pour le mettre */
+int		bt_fg(char **arg);
+int		bt_bg(char **arg);
+t_job   *get_job(char *arg);
+t_job   *get_job_plus();
+t_job   *get_job_minus();
+t_job   *get_job_id(int id);
+void	continue_job (t_job *j, int foreground);
+void	mark_job_as_running (t_job *j);
+void	put_job_in_foreground (t_job *j, int cont);
+void  	put_job_in_background (t_job *j, int cont);
+
+
+
+
+
+
+
 
 t_job	*find_job(pid_t pgid);
 int		job_is_stopped(t_job *j);
