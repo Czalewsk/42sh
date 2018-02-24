58558c471e87a011fb4f36cee6283651d2efd93c
Stephane CORBION <scorbion@e2r9p12.42.fr>
2018-02-24 10:13:36 +0100

    del one empty line at the end of the file

--- srcs/jobControl/continue_job.c
+++ srcs/jobControl/continue_job.c
@@ -6,7 +6,7 @@
 /*   By: scorbion <scorbion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/03 16:40:25 by scorbion          #+#    #+#             */
-/*   Updated: 2018/02/17 16:08:06 by scorbion         ###   ########.fr       */
+/*   Updated: 2018/02/24 10:11:00 by scorbion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,3 @@ void  continue_job (t_job *j, int foreground)
   else
     put_job_in_background (j, 1);
 }
-
