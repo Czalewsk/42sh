5a63c8faca2126182561da90b85309c002beda47
Timothee Hugo <thugo@student.42.fr>
2018-02-27 17:52:09 +0100

    WIP: make builtin cd

---
+++ srcs/builtins/builtin_cd.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_cd.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thugo <thugo@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/27 14:20:02 by thugo             #+#    #+#             */
+/*   Updated: 2018/02/27 17:23:30 by thugo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_sh.h"
+
+static char	*resolve_path(int argc, char **argv, int index)
+{
+	char	*path;
+
+	if (index >= argc)
+	{
+		if (!(path = ft_getenv(
+	}
+}
+
+int			builtin_cd(t_process *p)
+{
+	char	*options;
+	int		i;
+	char	*path;
+	char	*lpath;
+
+	if ((i = ft_getopt(ft_tab2dlen(p->argv), p->argv, "LP", &options)) == -1)
+		return (sh_error_bi(...));
+	if (!(path = resolve_path(ft_tab2dlen(p->argv), p->argv, index)))
+		return (EXIT_FAILURE);
+	if (ft_strchr(options, 'P'))
+		ret = changedir(path, NULL);
+	else
+	{
+		lpath = get_lpath(path);
+		ret = changedir(lpath, path);
+		free(lpath);
+	}
+	free(options);
+	return (ret);
+}
