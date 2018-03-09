d0c6d5299a2677c98568b1b12a686a83ceedbee8
Timothee Hugo <thugo@student.42.fr>
2018-03-09 09:39:08 +0100

    Fix norme

--- srcs/builtins/builtin_unsetenv.c
+++ srcs/builtins/builtin_unsetenv.c
@@ -6,7 +6,7 @@
 /*   By: thugo <thugo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/09 07:32:03 by thugo             #+#    #+#             */
-/*   Updated: 2018/03/09 08:04:41 by thugo            ###   ########.fr       */
+/*   Updated: 2018/03/09 09:38:52 by thugo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ int		builtin_unsetenv(t_process *p, int argc, char **argv, char **env)
 {
 	(void)env;
 	if (argc == 1)
+	{
 		return (sh_error_bi(p->stderr, EXIT_FAILURE, 1,
 			"unsetenv: Too few arguments.\n"));
+	}
 	while (*(++argv))
 		env_unset(*argv);
 	return (EXIT_SUCCESS);
