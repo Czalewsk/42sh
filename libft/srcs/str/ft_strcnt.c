b8be196e1d6f5f222aa71af4627a0b8b6f76dca7
Timothee Hugo <thugo@student.42.fr>
2018-03-15 03:10:21 +0100

    Fix norm error

--- libft/srcs/str/ft_strcnt.c
+++ libft/srcs/str/ft_strcnt.c
@@ -6,7 +6,7 @@
 /*   By: thugo <thugo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/03 03:09:04 by thugo             #+#    #+#             */
-/*   Updated: 2018/02/03 03:20:12 by thugo            ###   ########.fr       */
+/*   Updated: 2018/03/15 03:10:07 by thugo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@
 **  Renvoi la premiere occurence trouve dans s d'un des caractere de sfind.
 */
 
-char       *ft_strcnt(const char *s, const char *sfind)
+char	*ft_strcnt(const char *s, const char *sfind)
 {
-    if (!s || !sfind)
-        return (NULL);
-    while (*s)
-    {
-        if (ft_strchr(sfind, *s))
-            return ((char *)s);
-        ++s;
-    }
-    return (NULL);
+	if (!s || !sfind)
+		return (NULL);
+	while (*s)
+	{
+		if (ft_strchr(sfind, *s))
+			return ((char *)s);
+		++s;
+	}
+	return (NULL);
 }
