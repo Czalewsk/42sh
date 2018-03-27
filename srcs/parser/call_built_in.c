39eb2f08efccfff3cc2326ce4e145ff423a35f78
Dricc <czalewsk@student.42.fr>
2018-03-27 09:40:01 +0200

    handle error in modify io in bultin

--- srcs/parser/call_built_in.c
+++ srcs/parser/call_built_in.c
@@ -6,7 +6,7 @@
 /*   By: scorbion <scorbion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/24 23:24:26 by maastie           #+#    #+#             */
-/*   Updated: 2018/03/26 21:13:12 by czalewsk         ###   ########.fr       */
+/*   Updated: 2018/03/27 09:34:02 by czalewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,9 +38,8 @@ int		do_built_in(t_process *p, char **env)
 	{
 		if (!ft_strcmp(g_builtins[i].name, p->argv[0]))
 		{
-			modify_io_child(p);
-			g_sh.exitstatus = g_builtins[i].f(p,
-					ft_tab2dlen((const void **)p->argv), p->argv, env);
+			g_sh.exitstatus = (modify_io_child(p)) ? g_builtins[i].f(p,
+					ft_tab2dlen((const void **)p->argv), p->argv, env) : 0;
 			reset_fd(p->open_fd);
 			return (1);
 		}
