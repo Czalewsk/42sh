1776b5741e1fbaf7ba874f17f41b4ac1c0db5803
Dricc <czalewsk@student.42.fr>
2018-03-27 09:43:02 +0200

    normed in execution

--- srcs/parser/execution.c
+++ srcs/parser/execution.c
@@ -6,7 +6,7 @@
 /*   By: maastie <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/20 16:06:24 by maastie           #+#    #+#             */
-/*   Updated: 2018/03/27 09:26:53 by czalewsk         ###   ########.fr       */
+/*   Updated: 2018/03/27 09:42:50 by czalewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,6 @@ int				executor(t_job *j, t_process *p, int pipe[2][2], char **env)
 	else if ((exec_line = find_path(p->argv[0], ft_getenv(env, "PATH"))))
 	{
 		exec_with_acces(exec_line, p, j, env);
-		ft_strdel(&exec_line);
 		exit(g_sh.exitstatus);
 	}
 	ft_strdel(&exec_line);
